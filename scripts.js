3fe9fb9511939be874a13c2f4e7e7adae712232e
Magua William <williammagua@gmail.com>
2024-09-14 20:00:07 +0300

    Added visibility to the email on click

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 const cont = document.querySelector(".lets-talk");
 const contact = document.querySelector(".contact-info p");
+const email = document.querySelector(".contact-info a");
 
 let isNumberShown = false; // To track the state
 
 cont.addEventListener("click", () => {
   if (!isNumberShown) {
     contact.textContent = "+254 74656 9025";
-    cont.textContent = "Hide Number";
+    email.style.display = "block";
+    email.textContent = "support@MaguaWilliam.dev";
     isNumberShown = true;
-  } else {
-    contact.textContent = "+254 7**56 ***5";
-    cont.textContent = "Let's talk";
-    isNumberShown = false;
+
+    setTimeout(() => {
+      contact.textContent = "+254 7**56 ***5";
+      email.style.display = "none";
+      isNumberShown = false;
+    }, 7000);
   }
 });
